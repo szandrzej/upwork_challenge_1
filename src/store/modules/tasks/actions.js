69908ac14f209aa04b407fabc73345c736c19dbc
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-09-19 13:06:49 +0200

    Add implementation

--- src/store/modules/tasks/actions.js
+++ src/store/modules/tasks/actions.js
@@ -1,3 +1,50 @@
-export function addTask(task) {
+import { UPDATE_TASK, CREATE_TASK } from './types'
+import uuid from 'uuid/v4'
 
+export function addTask (name) {
+  return {
+    type: CREATE_TASK,
+    task: {
+      id: uuid(),
+      name,
+      isFinished: false,
+      notes: [],
+      finish: null,
+      start: null
+    }
+  }
+}
+
+export function stopTask (task) {
+  task = {
+    ...task,
+    start: task.start || Date.now(),
+    isFinished: true,
+    finish: Date.now()
+  }
+
+  return updateTask(task)
+}
+
+export function addNote (task, note) {
+  task = {
+    ...task,
+    notes: [ ...task.notes, note ]
+  }
+  return updateTask(task)
+}
+
+export function startTask (task) {
+  task = {
+    ...task,
+    start: Date.now()
+  }
+  return updateTask(task)
+}
+
+export function updateTask (task) {
+  return {
+    type: UPDATE_TASK,
+    task
+  }
 }
---
+++ src/store/modules/tasks/types.js
@@ -0,0 +1,2 @@
+export const UPDATE_TASK = 'UPDATE_TASK'
+export const CREATE_TASK = 'CREATE_TASK'
